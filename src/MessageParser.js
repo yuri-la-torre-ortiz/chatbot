9059119a18b7af1083935b76d212a3f3d2975ba0
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-14 16:24:02 +0200

    Checks if user message includes hello

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -1,12 +1,15 @@
 class MessageParser {
-    constructor(actionProvider, state) {
-      this.actionProvider = actionProvider;
-      this.state = state;
-    }
-  
-    parse(message) {
-      console.log(message)
-    }
-  }
+    constructor(actionProvider) {
+        this.actionProvider = actionProvider;
+      }
+    
+      parse(message) {
+        const lowerCaseMessage = message.toLowerCase()
+        
+        if (lowerCaseMessage.includes("hello")) {
+          this.actionProvider.greet()
+        }
+      }
+}
   
 export default MessageParser;
